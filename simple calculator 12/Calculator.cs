eff14bc88fe5a91f6066d4a97200d3f7a1087c7f
HeatherDa <crystal_tree@hotmail.com>
2017-03-29 06:11:53 -0500

    added calculator class and some class code

---
+++ simple calculator 12/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calculator_12
+{
+    class Calculator
+    {
+        private string CurrentValue = "";//it's what's displayed in the txtbox, so string
+        public Calculator() { }//empty constructor.  do I need it?
+        public Calculator(string input)//for using with typed input
+        {
+            CurrentValue = input;
+            parseInput(input);//create List of calculation elements
+        }
+
+        private void parseInput(string input)
+        {
+            String[] numbers = input.Split('+', '-', '/', '*');//string of input would, theoretically, only contain characters that exist on the keyboard
+
+            foreach (char a in input)
+            {
+                string num1 = "";
+                List<string> operators = new List<string>();
+                if (Operator(a)) operators.Add(Convert.ToString(a));//collect operators
+            }
+        }
+        private Boolean Operator(char a)
+        {
+            bool isOperator= false;
+            string operators = "/*-+";
+            foreach(char b in operators)
+            {
+
+                if (a.Equals(b)){
+                    isOperator = true;
+                }
+            }
+
+            return isOperator;
+                
+
+        }
+    }
+}
